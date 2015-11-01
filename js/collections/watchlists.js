f48a17c61672412bc7fa145f58cd07426ec8aa12
sam_begin <beginsam2@gmail.com>
2015-11-01 13:54:54 -0500

    changed templte disposition

--- js/collections/watchlists.js
+++ js/collections/watchlists.js
@@ -6,6 +6,12 @@ var app = app || {};
     var Watchlists = Backbone.Collection.extend({
         model: app.Watchlist,
         url: 'http://umovie.herokuapp.com/unsecure/watchlists',
+        byUser: function (userMail){
+            filteredCollection = this.filter(function(watchList){
+                return watchList.owner.email === userMail;
+            });
+            return new Watchlists(filteredCollection);
+        }
     });
 
     app.Watchlists = new Watchlists();
