c292bb3e4803d2d719aaf9c447f6fb190c0692a9
PhilKelly <phil.kelly.52@gmail.com>
2015-11-05 20:14:45 -0500

    removing comment

--- js/UI/menuBehavior.js
+++ js/UI/menuBehavior.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $('.nav.pull-left a').on('click', function(){
-        $('.btn-navbar').click() //bootstrap 3.x by Richard
+        $('.btn-navbar').click();
     });
 });
