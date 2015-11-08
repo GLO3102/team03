3c94617d465ba3fed83735d53638294327f19b73
gdemers <rurdsa@gmail.com>
2015-11-08 12:04:25 -0500

    Base pour recherche Youtube.

---
+++ js/search.js
@@ -0,0 +1,23 @@
+function gAPIOnLoadCallback() {
+    gapi.client.load('youtube', 'v3', youtubeApiLoadCallback);
+}
+
+function youtubeApiLoadCallback() {
+    gapi.client.setApiKey('AIzaSyCc31z5nGanStEs8UBJGIeCfDFI4syvRO4');
+
+    // Juste pour tester, a enlever
+    youtubeSearch();
+}
+
+function youtubeSearch(query) {
+    var request = gapi.client.youtube.search.list({
+        part: 'id',
+        q: query
+    });
+
+    request.execute(youtubeSearchResponse);
+}
+
+function youtubeSearchResponse(response) {
+    console.log(response);
+}
